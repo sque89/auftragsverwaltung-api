d01c3a3aebdeb3de535fc19cb8ccba35ae01ec02
Robin Kählert <robin@s-kaehlert.de>
2018-10-18 17:18:21 +0200

    implement logging and log controller

---
+++ src/Controller/LogController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Psr\Log\LoggerInterface;
+
+class LogController extends Controller {
+
+    private $entityManager;
+    private $serializer;
+
+    public function __construct() {
+    }
+
+    /**
+     * @Route("/api/log", name="logError", methods="PUT")
+     */
+    public function logError(Request $request, LoggerInterface $logger) {
+        try {
+            $requestData = json_decode($request->getContent(), true);
+            $logger->error($requestData['message'], array(
+                'application' => 'ng',
+                'callStack' => $requestData['callStack']
+            ));
+            return $this->json(array('code' => 200), 200);
+        } catch (Exception $ex) {
+            return $this->json(array('code' => 500, 'message' => $ex->getMessage()), 500);
+        }
+    }
+}
